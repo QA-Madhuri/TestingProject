75360a89263cbd21d7dd4af5fa56fe81e418cdc4
sk <sk@192.168.1.141>
2021-10-01 16:41:52 +0530

    Added Democlass

---
+++ Trespository/src/Democlass.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Democlass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\sk\\Desktop\\Selenium\\chromedriver\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com");}
+
+}
